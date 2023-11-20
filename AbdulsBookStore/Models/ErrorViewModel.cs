2b0d3349d75bd54345a53a282b94700c8ed3d0c3
W0786364 <W0786364@WDL-A0336Z-34>
2023-11-20 16:29:05 -0500

    Add project files.

---
+++ AbdulsBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AbdulsBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
