b4f25f3872e6ee9e2fc70872878d6326174cea29
W0786364 <W0786364@myscc.ca>
2023-11-20 17:48:46 -0500

    Created a category table

--- AbdulsBooks.DataAccess/Data/ApplicationDbContext.cs
+++ AbdulsBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AbdulsBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace AbdulsBookStore.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
