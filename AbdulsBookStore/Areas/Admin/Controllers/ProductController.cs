732c60370f9cfb31f06ee493ba0c1199f9e2bd8b
W0786364 <W0786364@ADL-A1201H-18>
2023-11-21 11:28:07 -0500

    i have add a Prodcutcontoller to contoller file and type the code

---
+++ AbdulsBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using AbdulsBooks.DataAccess.Repository.IRepository;
+using AbdulsBooks.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbdulsBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
